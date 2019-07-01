960b76b6525a5ec31560a9bd8c9d9cbe4b4b782e
ebonyhope <blessyn2hope@gmail.com>
2019-07-01 04:46:07 +0100

    completed the page

--- js/main.js
+++ js/main.js
@@ -1,88 +1,132 @@
-$(function() {
-  "use strict";
+var processBurstTimes = [];
+var lastProcessID = 1;
 
-  var nav_offset_top = $('header').height() + 50; 
-    /*-------------------------------------------------------------------------------
-	  Navbar 
-	-------------------------------------------------------------------------------*/
-    function navbarFixed(){
-        if ( $('.header_area').length ){ 
-            $(window).scroll(function() {
-                var scroll = $(window).scrollTop();   
-                if (scroll >= nav_offset_top ) {
-                    $(".header_area").addClass("navbar_fixed");
-                } else {
-                    $(".header_area").removeClass("navbar_fixed");
-                }
-            });
-        };
-    };
-    navbarFixed();
+function goToPage2(){
+    console.log($("#noOfProcesses").val());
 
-
-
-    /*-------------------------------------------------------------------------------
-	  clients logo slider
-	-------------------------------------------------------------------------------*/
-    if ($('.clients_slider').length) {
-      $('.clients_slider').owlCarousel({
-          loop: true,
-          margin: 30,
-          items: 5,
-          nav: false,
-          dots: false,
-          responsiveClass: true,
-          autoplay: 2500,
-          slideSpeed: 300,
-          paginationSpeed: 500,
-          responsive: {
-              0: {
-                  items: 1,
-              },
-              768: {
-                  items: 3,
-              },
-              1024: {
-                  items: 4,
-              },
-              1224: {
-                  items: 5
-              }
-          }
-      })
+    if(($("#noOfProcesses").val().length == 0)){
+        $("#noOfProcessesError").text("please input a valid number");
+        return;
     }
 
+    var noOfProcesses = parseInt($("#noOfProcesses").val(), 10);
 
-    /*-------------------------------------------------------------------------------
-	  testimonial slider
-	-------------------------------------------------------------------------------*/
-    if ($('.testimonial').length) {
-      $('.testimonial').owlCarousel({
-          loop: true,
-          margin: 30,
-          items: 5,
-          nav: false,
-          dots: true,
-          responsiveClass: true,
-          slideSpeed: 300,
-          paginationSpeed: 500,
-          responsive: {
-              0: {
-                  items: 1
-              }
-          }
-      })
+    if((noOfProcesses > 10)){
+        $("#noOfProcessesError").text("Number of processes should be less than 10");
+        return;
+    }    
+
+    $("#noOfProcessesError").text("");
+
+    $('#exampleModal').modal('hide');
+    $("#page1").hide();
+    $("#page2").fadeIn("slow");
+
+    for (i = 0; i < noOfProcesses; i++) { 
+        var row = `<tr>
+                        <td>${i+1}</td>
+                        <td><input type="number" class="form-control" name="burstTimes[]"></td>
+                    </tr>`;
+        $("#processEntry").append(row);
+    }
+}
+
+function goToPage3(){
+
+    processBurstTimes = [];
+
+    var burstTimes = $('input[name^=burstTimes]').map(function(idx, elem) {
+        return $(elem).val();
+    }).get();
+
+    console.log(burstTimes);
+    
+    for(burstTime of burstTimes){
+        if(burstTime.length == 0){
+            $("#processEntryError").text("please fill all fields with a valid number");
+            return;
+        }
+    }
+    $("#processEntryError").text("");
+
+    for(burstTime of burstTimes){
+        var process = {};
+        process.ID = lastProcessID;
+        process.BurstTime = parseInt(burstTime, 10);
+        processBurstTimes.push(process);
+
+        lastProcessID++;
     }
 
+    console.log(processBurstTimes);
 
-  /*-------------------------------------------------------------------------------
-	  Mailchimp 
-	-------------------------------------------------------------------------------*/
-	function mailChimp() {
-		$('#mc_embed_signup').find('form').ajaxChimp();
-	}
-  mailChimp();
-  
-});
+    $("#page2").hide();
+    $("#page3").fadeIn("slow");
 
+    createProcessTable();
+    calculateAverageWaitingTime();
+    getNewProcessID();
+    startAnimation();
+}
 
+function createProcessTable(){
+    $("#processTable").html("");
+    for (i = 0; i < processBurstTimes.length; i++) { 
+        var row = `<tr>
+                        <td>${processBurstTimes[i].ID}</td>
+                        <td>${processBurstTimes[i].BurstTime}</td>
+                    </tr>`;
+        $("#processTable").append(row);
+    }
+}
+
+function calculateAverageWaitingTime(){
+    var total = 0;
+    var n = processBurstTimes.length;
+
+    for (i = 0; i < n; i++) { 
+        total += processBurstTimes[i].BurstTime;
+    }
+
+    var average =  (total / n).toFixed(2);
+
+    $("#averageTime").text(`Average Waiting Time: ${average}`);
+}
+
+function getNewProcessID(){
+
+    $("#newProcess").text(`Process #: ${lastProcessID}`);
+}
+
+function startAnimation(){
+    //isaac insert your js code here
+    processBurstTimes.shift();
+}
+
+function addNewProcess(){
+    console.log($("#newBurstTime").val());
+
+    if(($("#newBurstTime").val().length == 0)){
+        $("#newProcessError").text("please input a valid number");
+        return;
+    }
+
+    var newBurstTime = parseInt($("#newBurstTime").val(), 10);
+
+    process = {};
+    process.ID = lastProcessID;
+    process.BurstTime = newBurstTime;
+
+    processBurstTimes.push(process);
+
+    lastProcessID++;
+
+    $('#exampleModal2').modal('hide');
+
+    $("#page3").fadeIn("slow");
+
+    createProcessTable();
+    calculateAverageWaitingTime();
+    getNewProcessID();
+    startAnimation();
+}
